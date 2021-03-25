e9cafd37fbb28b6b987d9139400d5ab219febc80
Mudassir-razi <54622087+Mudassir-razi@users.noreply.github.com>
2021-03-25 21:52:11 +0600

    Update CircuitError.cs

--- Grid/CircuitError.cs
+++ Grid/CircuitError.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Grid
 {
+    //Errors that can take place in circuit
     class CircuitError
     {
         public string Message { get; private set; }
